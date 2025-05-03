e552020437d777255fd5f1315b9119ac520bbeb9
julian7ch <jucalderonhe@unal.edu.co>
2025-05-03 12:31:25 -0500

    renombre a metodo split_into_words

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -4,7 +4,7 @@ import sys
 from ._internals.count_words import count_words
 from ._internals.preprocess_lines import preprocess_lines
 from ._internals.read_all_lines import read_all_lines
-from ._internals.split_in_words import split_in_words
+from ._internals.split_into_words import split_into_words
 from ._internals.write_word_counts import write_word_counts
 
 
@@ -19,7 +19,7 @@ def main():
 
     all_lines = read_all_lines(input_folder)
     all_lines = preprocess_lines(all_lines)
-    words = split_in_words(all_lines)
+    words = split_into_words(all_lines)
     counter = count_words(words)
     write_word_counts(counter, output_folder)
 
