e0c388b3f4a50f32ceb3ce6231cad1992bfe26a6
julian7ch <jucalderonhe@unal.edu.co>
2025-05-03 12:20:45 -0500

    ajuste adicional de funciones

--- homework/src/_internals/split_into_words.py
+++ homework/src/_internals/split_into_words.py
@@ -1,7 +1,7 @@
 import os
 
 
-def split_in_words(all_lines):
+def split_into_words(all_lines):
     words = []
     for line in all_lines:
         words.extend(word.strip(",.!?") for word in line.split())
