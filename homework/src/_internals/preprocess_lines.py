6b932577b08eb9be37c88269f2804883344ac3e0
julian7ch <jucalderonhe@unal.edu.co>
2025-05-03 11:00:42 -0500

    separación de funciones en archivos

---
+++ homework/src/_internals/preprocess_lines.py
@@ -0,0 +1,6 @@
+import os
+
+
+def preprocess_lines(all_lines):
+    all_lines = [line.lower().strip() for line in all_lines]
+    return all_lines
---
+++ homework/src/_internals/split_in_words.py
@@ -0,0 +1,8 @@
+import os
+
+
+def split_in_words(all_lines):
+    words = []
+    for line in all_lines:
+        words.extend(word.strip(",.!?") for word in line.split())
+    return words
